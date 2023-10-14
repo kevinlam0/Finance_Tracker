c1c11e2ad3abbc63567e18aea06984d3d6288f82
kevinlam0 <kevinclam02@gmail.com>
2023-10-14 11:00:31 -0400

    Got the connection working

---
+++ Main.py
@@ -0,0 +1,17 @@
+import csv
+import gspread
+import sys
+
+file = 'june2022.csv'
+file_path = './checkingsData/june2022.csv'
+print(file_path)
+with open(file_path, 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    for row in csv_reader:
+        print(row)
+        
+sa = gspread.service_account()
+sh = sa.open("Finance Tracker 2")
+wks = sh.worksheet("Sheet1")
+rows = None
+wks.insert_row([1,3], 10)
