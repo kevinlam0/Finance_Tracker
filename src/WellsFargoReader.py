e90d2e69221ab64959b07e715822661488b46e5a
kevinlam0 <kevinclam02@gmail.com>
2023-10-19 16:10:54 -0400

    Implemented TransactionReader for WellsFargo

---
+++ src/WellsFargoReader.py
@@ -0,0 +1,46 @@
+from TransactionReader import TransactionReader
+import csv
+import personalization
+import Venmo_Reader
+
+class WellsFargoReader(TransactionReader):
+    def __new__(cls, *args, **kwargs):
+        instance = super(WellsFargoReader, cls).__new__(cls)
+        return instance
+    
+    def __init__(self):
+        pass
+    
+    def format_rows_from_csv_file(self, file: str) -> list:
+        transactions = []
+        with open(file, 'r') as csv_file:
+            csv_reader = csv.reader(csv_file)
+            for row in csv_reader: 
+                # Need these
+                date = row[0]
+                amount = float(row[1])
+                
+                # Find the description of the transaction.
+                # Could be a spending or an income 
+                if "VENMO" in row[4]: 
+                    desc = "Venmo transaction: " + Venmo_Reader.find_venmo_transaction_description(amount, date) 
+                else: 
+                    desc = self.__trim_description(row[4])
+                
+                if amount > 0: category = "Venmo" if "Venmo" in desc else "Work income"
+                category = super()._find_category(desc, amount, date)
+                transaction: tuple = ((date, amount, desc, category))
+                transactions.append(transaction)
+                
+        return transactions
+
+    def __trim_description(self, description: str) -> str:
+        desc = description.split()
+        if "PURCHASE AUTHORIZED" in description:
+            desc = desc[4:]
+            
+        if "CARD" in desc and personalization.card_ending in desc:
+            desc = desc[:-3]
+            
+        return  " ".join(str(element) for element in desc)
+
