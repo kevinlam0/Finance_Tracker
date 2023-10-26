2aafdbe07b6cca654e6e8c8dd40a363248fc1595
kevinlam0 <kevinclam02@gmail.com>
2023-10-26 12:41:24 -0400

    Made venmoreader be an object with set data

--- src/VenmoReader.py
+++ src/VenmoReader.py
@@ -1,59 +1,66 @@
 import calendar, os, sys
 fpath = os.path.join(os.path.dirname(__file__), "../")
 sys.path.append(fpath)
-from src.CustomExceptions.MonthNotFoundError import IllegalDateError
+from src.CustomExceptions.IllegalDateError import IllegalDateError
 OLDEST_YEAR = 2015
 NEWEST_YEAR = 2025
 MONTHS = set([calendar.month_name[i].lower() for i in range(1, 13)])
 
 class VenmoReader():
     __data: list
+    __year: int
+    __month: str
     def __init__(self, file):
-        venmo_file = find_file(file)
-        self.__data = __read_csv_file(venmo_file)
-
-def find_venmo_description(amount: float, date: str) -> str:
-    return "Venmo transaction: " + find_venmo_transaction_description(amount, date)
-
-
-def find_venmo_transaction_description(amount: float, date: str) -> str:
-    """
-    Date must be in mm/dd/yyyy format
-    """
-    # Get the file path of the correct venmo month data
-    month = calendar.month_name[int(date[:2])].lower()
-    day = int(date[3:5])
-    if not(1<= day <= 31): raise IllegalDateError(f"The day of this date is not a real day: {date}")
-    year = date[-4:]
-    if not (OLDEST_YEAR <= int(year) <= NEWEST_YEAR): raise IllegalDateError(f"The year of this date is outside the range: {date}")
-    
-    # Find the correct transaction
-    for i in range(len(lines)):
-        row: list = lines[i].split(",")
-        venmo_amount: float = float(row[8][0] + row[8][3:])
-        venmo_day: int = int(row[2][8:10])
-        if venmo_amount == amount and abs(day - venmo_day) < 2: 
-            return f"\"{row[5]}\" to {row[7]}" 
+        self.__year = _find_year(file)
+        self.__month = _find_month(file)
+        venmo_file = find_file(self.__month, self.__year)
+        self.__data = _read_csv_file(venmo_file)
+    def find_venmo_description(self, amount: float, date: str) -> str:
+        return "Venmo transaction: " + self.find_venmo_transaction_description(amount, date) 
+    def find_venmo_transaction_description(self, amount: float, date: str) -> str:
+        """
+        Date must be in mm/dd/yyyy format
+        """
+        # Find the day of this transaction
+        day = int(date[3:5])
+        if not(1<= day <= 31): raise IllegalDateError(f"The day of this date is not a real day: {date}")
         
-    raise Exception("This is the transaction: " + str((month, day, year, amount)))
+        # --- Identifying the correct transaction --- #
+        # Buffer for transaction processing lag
+        delta = 1
+        while delta < 3:
+            for i in range(len(self.__data)):
+                # Identify amount 
+                row: list = self.__data[i].split(",")
+                venmo_amount: float = float(row[8][0] + row[8][3:])
+                if amount != venmo_amount: continue
+                
+                # Identify the day
+                venmo_day: int = int(row[2][8:10])
+                if abs(venmo_day - day) < delta:
+                    self.__data.pop(i)
+                    return f"\"{row[5]}\" to {row[7]}"
+            delta+=1
+        
+        raise Exception("This is the transaction: " + str((date, amount)))
 
-def __read_csv_file(file_path: str) -> list:
+
+def _read_csv_file(file_path: str) -> list:
     file = open(file_path, 'r')
     lines = [line.strip() for line in file]
     lines = lines[4:-27]
     return lines 
-
-def find_file(file: str) -> str:
+def _find_year(file: str) -> int:
     i = file.find("2")
-    year = file[i:i+4]
+    year = file[i : i+4]
     try: year = int(year)
     except ValueError: raise IllegalDateError(f"This file does not have a valid year: {year}")
     if not (OLDEST_YEAR <= year <= NEWEST_YEAR): raise IllegalDateError(f"This file is outside of the valid years {OLDEST_YEAR} - {NEWEST_YEAR}: {year}")
-        
-    month = ""
+    return year
+def _find_month(file: str) -> str:
     for m in MONTHS:
-        if m in file:
-            month = m
-            break
-    if month == "": raise IllegalDateError(f"This file does not have a valid month: {file}")
+        if m in file: 
+            return m
+    raise IllegalDateError(f"This file does not have a valid month: {file}")
+def find_file(month: str, year: int) -> str:
     return f"./venmoData/venmo{month}{year}.csv"
