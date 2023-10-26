004ac559590f7de94299e67c466fd35391cabe5c
kevinlam0 <kevinclam02@gmail.com>
2023-10-26 16:29:55 -0400

    changed name of files

--- src/Main.py
+++ src/Main.py
@@ -13,17 +13,17 @@ If you want to inject all data of a folder
 Args:
     folder (str): Name of folder with CSV data files in there
 """
-file_path = './checkingsData/WFseptember2022.csv'
+file_path = './2023checkingsData/WFmay2023.csv'
 google_sheet = "Finance Tracker 2"
-sheet = "September 2022"
-folder = "checkingsData"
+sheet = "May 2023"
+folder = "2023checkingsData"
 
 
 
 if __name__ == "__main__":
     fa = FinanceAutomator.FinanceAutomator(google_sheet)
     fa.connect_to_google()
-    # fa.inject_one_file_to_sheet(sheet, file_path)
+    fa.inject_one_file_to_sheet(sheet, file_path)
     # fa.inject_all_data(folder)
     # file_path = "./venmoData/venmojune2022.csv"
     
