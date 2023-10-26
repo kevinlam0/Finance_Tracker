605d325307c8b4f948e43fd1bea7a637f53dd0dd
kevinlam0 <kevinclam02@gmail.com>
2023-10-26 11:20:48 -0400

    made fields private

--- src/DiscoverReader.py
+++ src/DiscoverReader.py
@@ -1,8 +1,9 @@
+from typing import Any
 from src.TransactionReader import TransactionReader
 import csv
 
 class DiscoverReader(TransactionReader):
-    card: str
+    __card: str
     
     def __new__(cls, *args, **kwargs):
         instance = super(DiscoverReader, cls).__new__(cls)
@@ -12,7 +13,10 @@ class DiscoverReader(TransactionReader):
         c = type.lower()
         if c != "credit" and c != "debit":
             raise Exception("The card type needs to be debit or credit.")
-        self.card = c.capitalize()
+        self.__card = c.capitalize()
         
     def format_rows_from_csv_file(self, file: str) -> list:
         pass
+    
+    def get_card(self):
+        return self.__card
