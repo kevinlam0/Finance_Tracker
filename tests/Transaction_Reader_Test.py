53ef9a1f18861254e2708a175f2c3aae631d312b
kevinlam0 <kevinclam02@gmail.com>
2023-10-26 01:05:54 -0400

    passed first test

--- tests/Transaction_Reader_Test.py
+++ tests/Transaction_Reader_Test.py
@@ -3,14 +3,16 @@ fpath = os.path.join(os.path.dirname(__file__), '../')
 sys.path.append(fpath)
 from src.Transaction_Reader import Transaction_Reader as tr 
 from src.Wells_Fargo_Reader import Wells_Fargo_Reader as wfr
+from src.TransactionReaderFactory import TransactionReaderFactory
 import unittest
 
 
 class Transaction_Reader_Tests(unittest.TestCase):
     
     def test_initialize(self):
-        result = tr.initialize("Wells Fargo")
+        result = TransactionReaderFactory.get_reader("wells fargo", 'credit')
         self.assertIsInstance(result, wfr)
+        
         
 if __name__ == "__main__":
     unittest.main()
